5c5bbc46fad35625ea31e6416618d8c5b40f5687
sohanemon <m.sohanemon@gmail.com>
2022-08-29 13:12:26 +0600

    footer added

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ function create(meal) {
           
             <label for=${
               meal.idMeal
-            } class="see-more w-full text-center cursor-pointer block rounded-md bg-blue-600 py-2 font-semibold text-blue-50 hover:text-white hover:bg-blue-500 hover:shadow-md duration-75">open modal</label>
+            } class="see-more w-full text-center cursor-pointer block rounded-md bg-blue-600 py-2 font-semibold text-blue-50 hover:text-white hover:bg-blue-500 hover:shadow-md duration-75">See More</label>
 
             <input type="checkbox" id=${meal.idMeal}  class="modal-toggle" />
             <label for=${meal.idMeal}  class="modal cursor-pointer">
@@ -46,7 +46,9 @@ function create(meal) {
                 <div class='p-4'>
                     <h1 class= 'text-2xl font-semibold'>${meal.strArea} ${
     meal.strMeal
-  }</h1>
+  }<a title='Visit ${meal.strSource}' href=${meal.strSource} target='_blank'>
+                 <svg class='w-6 fill-current hover:fill-blue-600 inline' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path d="M172.5 131.1C228.1 75.51 320.5 75.51 376.1 131.1C426.1 181.1 433.5 260.8 392.4 318.3L391.3 319.9C381 334.2 361 337.6 346.7 327.3C332.3 317 328.9 297 339.2 282.7L340.3 281.1C363.2 249 359.6 205.1 331.7 177.2C300.3 145.8 249.2 145.8 217.7 177.2L105.5 289.5C73.99 320.1 73.99 372 105.5 403.5C133.3 431.4 177.3 435 209.3 412.1L210.9 410.1C225.3 400.7 245.3 404 255.5 418.4C265.8 432.8 262.5 452.8 248.1 463.1L246.5 464.2C188.1 505.3 110.2 498.7 60.21 448.8C3.741 392.3 3.741 300.7 60.21 244.3L172.5 131.1zM467.5 380C411 436.5 319.5 436.5 263 380C213 330 206.5 251.2 247.6 193.7L248.7 192.1C258.1 177.8 278.1 174.4 293.3 184.7C307.7 194.1 311.1 214.1 300.8 229.3L299.7 230.9C276.8 262.1 280.4 306.9 308.3 334.8C339.7 366.2 390.8 366.2 422.3 334.8L534.5 222.5C566 191 566 139.1 534.5 108.5C506.7 80.63 462.7 76.99 430.7 99.9L429.1 101C414.7 111.3 394.7 107.1 384.5 93.58C374.2 79.2 377.5 59.21 391.9 48.94L393.5 47.82C451 6.731 529.8 13.25 579.8 63.24C636.3 119.7 636.3 211.3 579.8 267.7L467.5 380z"/></svg>
+            </a></h1>
                     <p>${meal.strTags ? meal.strTags : "No tags available"}</p>
                 </div>
                 <div class="overflow-x-auto">
@@ -61,6 +63,7 @@ function create(meal) {
                         <tbody></tbody>
                     </table>
                 </div>
+                <h1 class='text-center text-2xl font-semibold my-4 text-gray-600'>Video Tutorial</h1>
                 <iframe class='w-full aspect-video' src=${meal.strYoutube.replace(
                   "watch?v=",
                   "embed/"
